40c1d05c9fde610f3e35ffb3b17691eccecab78f
flyingbird <flyingbird69@gmail.com>
2016-01-03 18:21:55 +0900

    test

---
+++ src/com/poly_rhythm/www/currentTimeMillis/Convert.java
@@ -0,0 +1,23 @@
+package com.poly_rhythm.www.currentTimeMillis;
+
+import java.util.Date;
+
+/**
+ * Convert
+ * <p>
+ * Created by flyingbird on 12/31/15.
+ */
+public class Convert {
+
+    private Convert() {
+        super();
+    }
+
+    public static void main(String[] args) {
+
+        Date date = new Date(1451493540787L);
+        System.out.println(date);
+
+    }
+
+}
