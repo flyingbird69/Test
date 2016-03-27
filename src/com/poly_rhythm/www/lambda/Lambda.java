4c99d84f75b4017a48d74f9ab5974e2f48e07e1b
flyingbird <flyingbird69@gmail.com>
2016-03-28 01:15:11 +0900

    test

--- src/com/poly_rhythm/www/lambda/Lambda.java
+++ src/com/poly_rhythm/www/lambda/Lambda.java
@@ -7,6 +7,8 @@ package com.poly_rhythm.www.lambda;
  */
 public class Lambda {
 
+    private String hoge;
+
     /**
      * main
      *
@@ -14,15 +16,53 @@ public class Lambda {
      */
     public static void main(String[] args) {
 
-        Itfc itfc = () -> {
-            System.out.println("hoge");
-        };
+        String str = "string";
+
+        Itfc itfc = () -> System.out.println(str);
 
         itfc.hoge();
 //        System.out.println("executed hoge");
 //
 //        itfc.page();
 //        System.out.println("executed page");
+
+        Lambda lambda = new Lambda();
+        lambda.hoge = "this";
+        lambda.hoge();
+
+        OneParam oneParam = param -> System.out.println(param);
+        oneParam.hoge("param");
+
+        MultiParam multiParam = (param1, param2) -> System.out.println(param1 + param2);
+        multiParam.hoge("param1", "param2");
+
+        lambda.hoge2();
+
+    }
+
+    private void hoge() {
+
+        Itfc itfc = () -> System.out.println(this.hoge);
+
+        itfc.hoge();
+
+    }
+
+    private void hoge2() {
+
+        Counter i = new Counter();
+        Itfc itrc = () -> System.out.println(i.next());
+        for (int j = 0; j < 100; j++) {
+            itrc.hoge();
+        }
+
+    }
+
+    private class Counter {
+        private int i =0;
+        public int next() {
+            return this.i++;
+        }
     }
 
     /**
@@ -32,6 +72,14 @@ public class Lambda {
     public interface Itfc {
         void hoge();
 //        void page();
+    }
+
+    public interface OneParam {
+        void hoge(String string);
+    }
+
+    public interface MultiParam {
+        void hoge(String string1, String string2);
     }
 
 }
